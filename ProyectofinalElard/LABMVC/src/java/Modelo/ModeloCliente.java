28626ca12546830be635ad6e1b8a0db5c22ad47e
elard Rodriguez Marca <85025895+ElardRoma2021@users.noreply.github.com>
2022-07-05 15:07:41 -0500

    Add files via upload

---
+++ ProyectofinalElard/LABMVC/src/java/Modelo/ModeloCliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author elard
+ */
+import java.util.*;
+public class ModeloCliente {
+    public String cdni;
+    public String cnombre;
+    public String capellido;
+    public String cdireccion;
+    public String cemail;
+    public String ctelefono;
+    public String cclave;
+    
+    
+}
